89cfd562efc2a1f4ce89a870fa8259ef1db45a56
Camila Moura <camilapaleo@gmail.com>
2019-01-22 19:45:53 +0100

    change name folder, create error pages

--- company-blog/__init__.py
+++
@@ -1,3 +0,0 @@
-# companyblog/__init__.py
-from flask import Flask
-app = Flask(__name__)
---
+++ companyblog/__init__.py
@@ -0,0 +1,10 @@
+# companyblog/__init__.py
+from flask import Flask
+
+app = Flask(__name__)
+
+from companyblog.core.views import core
+from companyblog.error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
