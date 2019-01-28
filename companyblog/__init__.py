04439a12ba549f97063dddfd6e2f086a4463a9be
Camila Moura <camilapaleo@gmail.com>
2019-01-28 16:28:40 +0100

    init

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 
 from companyblog.core.views import core
 from companyblog.error_pages.handlers import error_pages
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+################
+#DATABASE SETUP#
+################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+from companyblog.core.views import core
+from companyblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
