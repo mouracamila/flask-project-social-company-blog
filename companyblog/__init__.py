542f72bb7d293410d6ebe1b94b5c14082dd87aec
Camila Moura <camilapaleo@gmail.com>
2019-01-31 18:06:03 +0100

    login

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -34,7 +34,9 @@ login_manager.login_view = 'user.login'
 ###############
 
 from companyblog.core.views import core
+from companyblog.users.views import users
 from companyblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
