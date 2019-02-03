332d24baa6198da044a2227129423338541583e0
Camila Moura <camilapaleo@gmail.com>
2019-02-03 18:12:46 +0100

    work in progress/templates

--- companyblog/core/views.py
+++ companyblog/core/views.py
@@ -1,13 +1,14 @@
 # core/views.py
-
+from companyblog.models import BlogPost
 from flask import render_template,request,Blueprint
 
 core = Blueprint('core',__name__)
 
 @core.route('/')
 def index():
-    #MORE TO COME!
-    return render_template('index.html')
+    page = request.args.get('page',1,type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date()).paginate(page=page,per_page=5)
+    return render_template('index.html',blog_posts=blog_posts)
 
 @core.route('/info')
 def info():
